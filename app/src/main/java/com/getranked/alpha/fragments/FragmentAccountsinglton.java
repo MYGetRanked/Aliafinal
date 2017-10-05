fc6bd0fa4cf7ec04e83277a455144b0d71206a6c
Usama <13besemiqbal@seecs.edu.pk>
2017-10-05 14:45:34 +0500

    Latest code till now

---
+++ app/src/main/java/com/getranked/alpha/fragments/FragmentAccountsinglton.java
@@ -0,0 +1,12 @@
+package com.getranked.alpha.fragments;
+
+import android.app.DownloadManager;
+
+/**
+ * Created by Usama on 10/5/2017.
+ */
+
+public class FragmentAccountsinglton {
+    private static FragmentAccountsinglton minstance;
+//    private Reuest.
+}
---
+++ app/src/main/java/com/getranked/alpha/models/MONfragmentAccountMainHome.java
@@ -0,0 +1,12 @@
+package com.getranked.alpha.models;
+
+/**
+ * Created by Usama on 10/5/2017.
+ */
+
+import com.getranked.alpha.restclient.BaseModel;
+
+public class MONfragmentAccountMainHome extends BaseModel {
+
+
+}
