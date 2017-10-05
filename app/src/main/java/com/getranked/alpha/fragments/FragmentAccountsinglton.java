3e2f68ebaa3f94cf1127bd4850248f44b2fdf29b
Usama <13besemiqbal@seecs.edu.pk>
2017-10-05 16:54:51 +0500

    Latest code till now Ijaz bhai +Me

--- app/src/main/java/com/getranked/alpha/fragments/FragmentAccountsinglton.java
+++ app/src/main/java/com/getranked/alpha/fragments/FragmentAccountsinglton.java
@@ -1,6 +1,11 @@
 package com.getranked.alpha.fragments;
 
 import android.app.DownloadManager;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 /**
  * Created by Usama on 10/5/2017.
@@ -8,5 +13,31 @@ import android.app.DownloadManager;
 
 public class FragmentAccountsinglton {
     private static FragmentAccountsinglton minstance;
-//    private Reuest.
+    private RequestQueue requestQueue;
+    private static Context mCtx;
+
+    private FragmentAccountsinglton(Context context){
+        mCtx =context;
+        requestQueue = getRequestQueue();
+    }
+    public RequestQueue getRequestQueue(){
+        if (requestQueue==null){
+            requestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+    public static synchronized FragmentAccountsinglton getInstance(Context context){
+        if(minstance==null){
+            minstance = new FragmentAccountsinglton(context);
+
+
+        }
+        return minstance;
+
+    }
+
+    public<T> void addToRequestque(Request<T> request){
+
+            requestQueue.add(request);
+    }
 }
